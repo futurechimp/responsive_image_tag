2c7c4179edeff2939e6e7bae1571a97a247c75bc
futurechimp <dave@toaster>
2011-08-16 11:44:24 +0100

    Working on generators a little.

---
+++ lib/rails/generators/responsive_image_tag/javascript/javascript_generator.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+module ResponsiveImageTag
+  module Generators
+    class JavascriptGenerator < Rails::Generators::Base
+      desc 'Creates a javascript file which takes care of responsive images for you'
+
+      def self.source_root
+        File.expand_path("../templates", __FILE__)
+      end
+
+      def create_javascript_file
+        template 'responsive-image-tag.js', 
+                  File.join('javascript', 'responsive-image-tag.js')
+      end
+      
+    end
+  end
+end
