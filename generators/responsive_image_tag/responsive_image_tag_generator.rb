6e68473c0b07c9c961696ed5659d47cd70011cad
futurechimp <dave@toaster>
2011-08-16 12:38:05 +0100

    Fixing up paths.

--- generators/responsive_image_tag/responsive_image_tag_generator.rb
+++ generators/responsive_image_tag/responsive_image_tag_generator.rb
@@ -1,10 +1,10 @@
 class ResponsiveImageTagGenerator < Rails::Generator::Base
 
-  JS_FILE = File.join("..", "..", "..", "public", "javascripts", "responsive-image-tag.js")
+  SOURCE = File.join("..", "..", "..", "lib", "rails", "generators", "responsive_image_tagBLAH", "javascript", "templates", "responsive-image-tag.js")
 
   def manifest
     record do |m|
-        m.file JS_FILE, "lib/rails/generators/responsive_image_tag/javascript/templates/responsive-image-tag.js"
+        m.file SOURCE, "public/javascripts/responsive-image-tag.js"
     end
   end
 
